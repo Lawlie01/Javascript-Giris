fc851e3cf5c8e26395ca3357db8b0aa6161958a1
Lawlie <ahmetsadikoglu977@gmail.com>
2023-03-24 16:15:43 +0300

    1. ders

---
+++ js egitim/01-hello-world.js
@@ -0,0 +1,5 @@
+// bu derste Hello World yani Merhaba dünya ile yazılıma giriş yapacağız. 3 şekilde hello world yazdırabiliriz.
+//     1. Console içinde - console.log("Hello World");
+//     2. alert paneli içinde - alert("Hello World");
+//     3. döküman içinde - document("Hello World");
+console.log("Hellow World")

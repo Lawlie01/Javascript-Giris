7d3f91a7a75acd43b3fdc2b51265dda49a11168b
Lawlie <ahmetsadikoglu977@gmail.com>
2023-03-25 15:19:39 +0300

    boolean-devam

--- js egitim/05-boolean-veri-turu-kullanimi.js
+++ js egitim/05-boolean-veri-turu-kullanimi.js
@@ -1,5 +1,7 @@
 // ******** Boolean veri turu ile calismak ********
 
+// Js Boolean iki değerden birini temsil eder. true veya false - 1 veya 0
+
 // 0 ve 1'i Anlamak -->
 let isActive = false // 0
 isActive = true // 1
@@ -22,6 +24,31 @@ console.log("User Name:", Boolean(userName))
 Boolean(0) // false
 Boolean(-0) // false
 Boolean(-0.1) //true
-Boolean(0 === 0) //true "===" eşit mi ibaresi taşır
+Boolean(0 === 0) //true "===" veri türleri eşit mi ibaresi taşır -- "==" eşit mi ibaresi taşır
 
 Boolean(userName.length > 0) // true
+
+let x=true;
+let y=false;
+
+        function verify(){
+            if(x==y){
+                document.getElementById("result").innerHTML="Bunlar Eşit";
+            } 
+            else{
+                document.getElementById("result").innerHTML="Bunlar Eşit değil";
+            }
+            
+        }
+
+    // function verify(){
+    //     document.getElementById("result").innerHTML=
+    //     Boolean(0)+"<br>"+ -- fale
+    //     Boolean(-0)+"<br>"+ -- false
+    //     Boolean("")+"<br>"+
+    //     Boolean(null)+"<br>"+ // null değeri boş demektir.
+    //     Boolean(false)+"<br>"+
+    //     Boolean(x)+"<br>"+
+    //     Boolean(y)+"<br>"+
+    //     Boolean(NaN)+"<br>"; Not a Number
+    // }

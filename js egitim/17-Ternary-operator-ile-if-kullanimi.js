cbe41d329d426da89aaeffd6fe3314d8b685b6ef
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-03 15:24:59 +0300

    ders-17

---
+++ js egitim/17-Ternary-operator-ile-if-kullanimi.js
@@ -0,0 +1,16 @@
+// ****************** Ternary Operator ile if kullanimi ******************
+
+
+
+
+
+
+
+let money = 40;
+let canBuy =
+
+    (money < 17) ? "Satin alamazsin.." :
+        (money > 30) ? "Satin alabilirsin.." :
+            "Para miktarini girmen gerekmektedir..";
+
+console.log(canBuy) // satin alabilirsin.

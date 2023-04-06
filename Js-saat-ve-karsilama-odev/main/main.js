c3aa98892767f0e56bf0ea8bd1c1c389e40e503f
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-06 15:48:07 +0300

    saat-eklendi-ek-duzeltmeler-yapildi

--- Js-saat-ve-karsilama-odev/main/main.js
+++ Js-saat-ve-karsilama-odev/main/main.js
@@ -1,8 +1,24 @@
 
+let firstName = prompt("Adınızı Giriniz: ");
+let showName = document.querySelector('#myName')
+showName.innerHTML = firstName;
+
+let clock = document.querySelector('.clock')
+
+tarihSaat()
 
 function tarihSaat() {
-    let date = new Date().toLocaleDateString("tr-TR");
-    document.getElementById("zaman").innerHTML = date;
+    let date = new Date();
+    let gunler = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    let gun = date.getDay();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+     let dsaat = hour + ":" + ((min < 10) ? '0' : '') + min + ":" + ((sec < 10) ? '0' : '') + sec + " " + gunler[gun];
+
+document.getElementById("myClock").innerHTML = dsaat;
 }
 
 setInterval(tarihSaat, 1000);
+

66470a1a364e5492f397582ff154e5bd4462fc3e
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-13 16:10:37 +0300

    ornekler-bitti

--- js egitim/20-Fonksiyonlara-Parametre-atamak.js
+++ js egitim/20-Fonksiyonlara-Parametre-atamak.js
@@ -8,9 +8,14 @@
 
 
 
+
+
+
+
 // Ornekler
 
-// 1--
+// 1-- Herhangi iki sayiyi toplayip, bir degiskene atayan fonksiyon ifadesini yazalim
+
 function toplama(a, b) { //a ve b toplanacak iki sayiyi temsil eden fonksiyon parametreleri(girdileri)
     let sonuc = a + b; // fonksiyonda aldigimiz parametlerle yaptigimiz islem
     return sonuc; // Herhangi iki sayinin toplamindan elde edecegimiz islem sonucunu, return ifadesinden hemen sonra belirtiyoruz.
@@ -19,7 +24,7 @@ function toplama(a, b) { //a ve b toplanacak iki sayiyi temsil eden fonksiyon pa
 var donenSonuc = toplama(5, 6);
 console.log("Toplam =", donenSonuc);
 
-// 2--
+// 2-- Herhangi bir yaricapa sahip bir dairenin alanini hesaplayan fonksiyonu yaziniz.
 
 const PI = 3.14; // Formulde kullandigimiz sabit sayi pi'yi bu sekilde alabiliriz
 
@@ -35,9 +40,24 @@ console.log("Daire Alani =", donenSonuc);
 
 // Kisaltilmisi
 
-function daireAlaniHesaplama2(r, PI = 3.14){
-    return PI*r*r;
+function daireAlaniHesaplama2(r, PI = 3.14) {
+    return PI * r * r;
 }
 
 var donenSonuc2 = daireAlaniHesaplama2(4)
 console.log("Daire Alani =", donenSonuc2)
+
+// 3-- Farkli yaricaplardaki iki farkli dairenin, alanlari carpimini veren bir fonksiyon yaziniz.
+
+function daireAlaniHesaplama3(r, PI = 3.14) {
+    return PI * r * r;
+}
+
+function carpma(a1, a2) {
+    return a1 * a2;
+}
+
+var alan1= daireAlaniHesaplama3(5); // 5 ve 6 argumanlari icin dairelerin alanlari hesaplandi.
+var alan2 = daireAlaniHesaplama3(6);
+var donenSonuc3 = carpma( alan1, alan2); // Hesaplanan alanlar arguman olarak verildi.
+console.log("Carpim =",donenSonuc3);

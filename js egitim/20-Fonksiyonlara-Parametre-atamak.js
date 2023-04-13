832483e8ec8af5777bbcc254a6b4d4cb163bb62a
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-13 16:02:34 +0300

    toplama-islemleri

--- js egitim/20-Fonksiyonlara-Parametre-atamak.js
+++ js egitim/20-Fonksiyonlara-Parametre-atamak.js
@@ -5,3 +5,39 @@
 // 2: Bir Fonksiyon Disari Bilgi Gonderebilir(return) veya Gondermeyebilir
 // 3: Mumkunse Fonksiyonun Bagimliliklarini Azaltmak Gerekir
 
+
+
+
+// Ornekler
+
+// 1--
+function toplama(a, b) { //a ve b toplanacak iki sayiyi temsil eden fonksiyon parametreleri(girdileri)
+    let sonuc = a + b; // fonksiyonda aldigimiz parametlerle yaptigimiz islem
+    return sonuc; // Herhangi iki sayinin toplamindan elde edecegimiz islem sonucunu, return ifadesinden hemen sonra belirtiyoruz.
+}
+
+var donenSonuc = toplama(5, 6);
+console.log("Toplam =", donenSonuc);
+
+// 2--
+
+const PI = 3.14; // Formulde kullandigimiz sabit sayi pi'yi bu sekilde alabiliriz
+
+function daireAlaniHesaplama(r) // Fonksiyonumuz, r parametresini aliyor.
+{
+    var islemSonucu = PI * r * r; // Daşrenşn akanşnş hesaokayacak islemimiz.
+    return islemSonucu; // return ifadesiyle sonucu donuyoruz.
+}
+
+var donenSonuc = daireAlaniHesaplama(3); //arguman oalrak, alanini hesaplamak istedigimiz herhangi bir dairenin yaricapi icin 3 rakami verildi.
+
+console.log("Daire Alani =", donenSonuc);
+
+// Kisaltilmisi
+
+function daireAlaniHesaplama2(r, PI = 3.14){
+    return PI*r*r;
+}
+
+var donenSonuc2 = daireAlaniHesaplama2(4)
+console.log("Daire Alani =", donenSonuc2)

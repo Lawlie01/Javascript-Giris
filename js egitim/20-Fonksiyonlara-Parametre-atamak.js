52d23a3f20ff319c29de53dacd3f4a5ed5b6f767
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-13 15:44:09 +0300

    ders-20

---
+++ js egitim/20-Fonksiyonlara-Parametre-atamak.js
@@ -0,0 +1,7 @@
+// ****************** Fonksiyonlara Parametlere Atamak ve Fonksiyondan Geri donus Almak ******************
+
+// Temel Kurallar:
+// 1: Bir fonksiyon Bir veya Birden Fazla Parametre Alabilir veya Hic Almayabilir
+// 2: Bir Fonksiyon Disari Bilgi Gonderebilir(return) veya Gondermeyebilir
+// 3: Mumkunse Fonksiyonun Bagimliliklarini Azaltmak Gerekir
+

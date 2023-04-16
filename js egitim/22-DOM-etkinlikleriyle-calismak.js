4564749b0a5b553386805e572ca29668338c6008
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-16 15:36:03 +0300

    ders-22

--- js egitim/22-DOM-etkinlikleriyle-calismak.js
+++ js egitim/22-DOM-etkinlikleriyle-calismak.js
@@ -1,19 +1,15 @@
 // DOM Etkinlikleriyle Calismak
 // https://www.w3schools.com/jsref/dom_obj_event.asp
+// https://www.w3bai.com/tr/index.html  3schools un turkce versiyonu
 
 let greeting = document.querySelector("#greeting")
 greeting.addEventListener("click", domClick) //fare isaretcisi ogeye geldiginde rengi degisir.
 
-function domClick(){
+function domClick() {
     console.log(event)
     this.style.color == "red" ? this.style.color = "black" : this.style.color = "red" //boylelikle rengi degisebiliyor
 }
 
 
 
-
-
-
-
-// Ornekler
 

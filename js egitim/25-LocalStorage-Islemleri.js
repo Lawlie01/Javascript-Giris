1e7f925474085da182769f33a4060ac604e18fc0
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-24 13:58:57 +0300

    ders-25

--- js egitim/25-LocalStorage-Islemleri.js
+++ js egitim/25-LocalStorage-Islemleri.js
@@ -1,1 +1,23 @@
 // ***************** LocalStorage Islemleri *****************
+
+
+
+
+
+
+// let userStatus = {userName: 'Leonhart', isActive: true}
+// localStorage.setItem('user', userStatus); 
+
+let user = { userName: "Lawlie", isActive: true }
+console.log(user)
+localStorage.setItem('userInfo', JSON.stringify(user));
+
+let userInfo = localStorage.getItem('userInfo'); // bilgiyi al
+userInfo = JSON.parse(userInfo)
+console.log(userInfo)
+
+let items = [1, 2, 3, user]
+localStorage.setItem('newItem', items)
+localStorage.setItem('newItem', JSON.stringify(items))
+localStorage.getItem('newItem')
+JSON.parse(localStorage.getItem('newItem'))
